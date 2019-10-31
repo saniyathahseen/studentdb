7829943241528c0f79c44618606ff1d6f2386515
Anusree <anusreeshibu1234@gmail.com>
2019-10-31 12:53:23 -0700

    Update viewstd.php

--- viewstd.php
+++ viewstd.php
@@ -1,4 +1,4 @@
-
+<center>
    
     <?php
     session_start();
@@ -10,19 +10,20 @@
     while ($row1 = mysqli_fetch_array($sql)){
         ?>
         <Form class="form-container"style="">
-        <span>Register Number:</span><span id="reg"></span><br><br> <?php echo $row1['Reg_No']; ?>
-        <span>First Name:</span><span id="fname"></span><br><br><?php echo $row1['F_name']; ?>
-        <span>Last Name:</span><span id="lname"></span><br><br><?php echo $row1['L_name']; ?>
-        <span>Date Of Birth:</span><span id="dob"></span><br><br><?php echo $row1['DOB']; ?>
-        <span>Sex:</span><span id="sex"></span><br><br><?php echo $row1['Sex']; ?>
-        <span>City:</span><span id="city"></span><br><br><?php echo $row1['City']; ?>
-        <span>District:</span><span id="dis"></span><br><br><?php echo $row1['District']; ?>
-        <span>State:</span><span id="state"></span><br><br><?php echo $row1['State']; ?>
-        <span>Course:</span><span id="course"></span><br><br><?php echo $row1['Course']; ?>
-        <span>Date Of Joining:<span id="doj"></span><br><br><?php echo $row1['DOJ']; ?>
-        <span>EmailId:</span><span id="email"></span><br><br><?php echo $row1['email']; ?>
-        <span>MobileNo:</span><span id="mob"></span><br><?php echo $row1['mob']; ?>
+        <span>Register Number:</span><span id="reg"></span> <?php echo $row1['Reg_No']; ?><br><br>
+        <span>First Name:</span><span id="fname"></span><?php echo $row1['F_name']; ?><br><br>
+        <span>Last Name:</span><span id="lname"></span><?php echo $row1['L_name']; ?><br><br>
+        <span>Date Of Birth:</span><span id="dob"></span><?php echo $row1['DOB']; ?><br><br>
+        <span>Sex:</span><span id="sex"></span><?php echo $row1['Sex']; ?><br><br>
+        <span>City:</span><span id="city"></span><?php echo $row1['City']; ?><br><br>
+        <span>District:</span><span id="dis"></span><?php echo $row1['District']; ?><br><br>
+        <span>State:</span><span id="state"></span><?php echo $row1['State']; ?><br><br>
+        <span>Course:</span><span id="course"></span><?php echo $row1['Course']; ?><br><br>
+        <span>Date Of Joining:<span id="doj"></span><?php echo $row1['DOJ']; ?><br><br>
+        <span>EmailId:</span><span id="email"></span><?php echo $row1['email']; ?><br><br>
+        <span>MobileNo:</span><span id="mob"></span><?php echo $row1['mob']; ?><br><br>
       </form>
       <?php
     }
     ?>
+    </center>
