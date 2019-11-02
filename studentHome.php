50a5dc6aff169efbfafda2537a23c91b0ab1db2f
Anusree <anusreeshibu1234@gmail.com>
2019-11-02 12:28:20 -0700

    Update studentHome.php

--- studentHome.php
+++ studentHome.php
@@ -21,6 +21,11 @@
     .container p{
       line-height: 200px; 
       }
+      .marq{
+        color:red;
+        margin-left:100px;
+        margin-top:50px;
+      }
     </style>
   </head>
   <body background="1d.jpg">
@@ -65,6 +70,12 @@
         </form> 
       </div>
     </nav>
+    <div class="marq">
+<marquee width="40%" direction="left" height="30%">
+    Education is the most Powerful weapon 
+   which you can use to change the world
+  </marquee>
+  </div>
     <div class="fixed-footer">
       <div class="container">For More Info:Conatact:+91485679</div>  
       <center>
